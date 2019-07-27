a384832df951b5dc8ed5e629a3e6aa6f9d8bd5cd
Evgeny Bobkin <evgen.ibqn@gmail.com>
2019-07-27 21:31:37 +0200

    add linting support

--- src/index.tsx
+++ src/index.tsx
@@ -32,7 +32,6 @@ import { IEditorServices } from '@jupyterlab/codeeditor';
 
 import '../style/index.css';
 
-
 /**
  * The CSS classes added to the cell footer.
  */
@@ -40,16 +39,16 @@ const CELL_FOOTER_CLASS = 'jp-CellFooter';
 const CELL_FOOTER_DIV_CLASS = 'ccb-cellFooterContainer';
 const CELL_FOOTER_BUTTON_CLASS = 'ccb-cellFooterBtn';
 
-
-function activate(app: JupyterFrontEnd, tracker: INotebookTracker): Promise<void> {
+function activateCommands(app: JupyterFrontEnd, tracker: INotebookTracker): Promise<void> {
+    // tslint:disable-next-line:no-console
     console.log('JupyterLab extension jupyterlab-cellcodebtn is activated!');
 
-    Promise.all([app.restored]).then(([args]) => {
+    Promise.all([app.restored]).then(([params]) => {
         const { commands, shell } = app;
 
         function getCurrent(args: ReadonlyJSONObject): NotebookPanel | null {
             const widget = tracker.currentWidget;
-            const activate = args['activate'] !== false;
+            const activate = args.activate !== false;
 
             if (activate && widget) {
                 shell.activateById(widget.id);
@@ -71,7 +70,7 @@ function activate(app: JupyterFrontEnd, tracker: INotebookTracker): Promise<void
                 if (current) {
                     const { context, content } = current;
                     NotebookActions.run(content, context.session);
-                    //current.content.mode = 'edit';
+                    // current.content.mode = 'edit';
                 }
             },
             isEnabled,
@@ -110,7 +109,7 @@ export class CellFooterWithButton extends ReactWidget implements ICellFooter {
     /**
      * Construct a new cell footer.
      */
-    constructor(commands: CommandRegistry,) {
+    constructor(commands: CommandRegistry) {
         super();
         this.addClass(CELL_FOOTER_CLASS);
         this.commands = commands;
@@ -124,7 +123,7 @@ export class CellFooterWithButton extends ReactWidget implements ICellFooter {
                 className={CELL_FOOTER_DIV_CLASS}>
                 <button
                     className={CELL_FOOTER_BUTTON_CLASS}
-                    onClick={(event) => {
+                    onClick={event => {
                         this.commands.execute('run-select-next-edit');
                     }}>
                     run
@@ -140,8 +139,8 @@ export class CellFooterWithButton extends ReactWidget implements ICellFooter {
 const footerButtonExtension: JupyterFrontEndPlugin<void> = {
     id: 'jupyterlab-cellcodebtn',
     autoStart: true,
-    activate: activate,
-    requires: [INotebookTracker]
+    activate: activateCommands,
+    requires: [INotebookTracker],
 };
 
 /**
@@ -153,21 +152,22 @@ const cellFactory: JupyterFrontEndPlugin<NotebookPanel.IContentFactory> = {
     requires: [IEditorServices],
     autoStart: true,
     activate: (app: JupyterFrontEnd, editorServices: IEditorServices) => {
+        // tslint:disable-next-line:no-console
         console.log(
             'JupyterLab extension jupyterlab-cellcodebtn',
-            'overrides default nootbook content factory'
+            'overrides default nootbook content factory',
         );
 
         const { commands } = app;
-        let editorFactory = editorServices.factoryService.newInlineEditor;
+        const editorFactory = editorServices.factoryService.newInlineEditor;
         return new ContentFactoryWithFooterButton(commands, { editorFactory });
-    }
+    },
 };
 
 /**
  * Export this plugins as default.
  */
-const plugins: JupyterFrontEndPlugin<any>[] = [
+const plugins: Array<JupyterFrontEndPlugin<any>> = [
     footerButtonExtension,
     cellFactory,
 ];
