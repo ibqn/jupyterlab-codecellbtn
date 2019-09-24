ea4bce6d24d5acc8d27e58b02a79315e34c2f24d
Evgeny Bobkin <evgeny.bobkin@stackfuel.com>
2019-09-24 17:34:54 +0200

    update jupyterlab script

--- jupyter.bash
+++ jupyter.bash
@@ -7,11 +7,11 @@ python3 -m venv './venv'
 source './venv/bin/activate'
 
 python3 -m pip install --upgrade pip
-#python3 -m pip install --upgrade setuptools
+python3 -m pip install --upgrade setuptools
 
 python3 -m pip install --upgrade --requirement './requirements.txt'
 
-exec jupyter notebook --no-browser --port 9999
+exec jupyter lab --port 9999 #--no-browser
 
 # you might need to install
 # sudo apt install python3-venv
